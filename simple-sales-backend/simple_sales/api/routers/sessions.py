523bbe8f2b8c54fc0b9c803aaf69732e3f43ba21
Kirill Gashkov <kirill@gashkov.com>
2022-12-06 20:56:17 +0300

    Outline 'users' and 'sessions'

--- simple-sales-backend/simple_sales/api/routers/sessions.py
+++ simple-sales-backend/simple_sales/api/routers/sessions.py
@@ -1,3 +1,23 @@
 import fastapi
 
 router = fastapi.APIRouter()
+
+
+@router.post("/sessions")
+def create_session() -> None:
+    # - Body: 'username' (required), 'password' (required)
+    # - Sets a session ID cookie
+    ...
+
+
+@router.delete("/sessions/current")
+def delete_current_session() -> None:
+    # - Authentication: required
+    ...
+
+
+@router.delete("/sessions")
+def delete_sessions() -> None:
+    # - Query parameters: 'user_id' (required) (could be 'current')
+    # - Authentication: required
+    ...
