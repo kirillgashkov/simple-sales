ffe3fababc52e373872460e67b8843a71daf7fcb
Kirill Gashkov <kirill@gashkov.com>
2022-12-06 20:54:55 +0300

    Add empty 'employees' router

--- simple-sales-backend/simple_sales/api/app.py
+++ simple-sales-backend/simple_sales/api/app.py
@@ -1,9 +1,10 @@
 import fastapi
 
-from simple_sales.api.routers import cities, sessions, users
+from simple_sales.api.routers import cities, employees, sessions, users
 
 app = fastapi.FastAPI()
 
 app.include_router(cities.router)
-app.include_router(users.router)
 app.include_router(sessions.router)
+app.include_router(users.router)
+app.include_router(employees.router)
