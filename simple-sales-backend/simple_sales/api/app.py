c54ce58212602d862c2d94ca38773a8b058bc5c6
Kirill Gashkov <k.e.gashkov@gmail.com>
2022-12-06 20:52:58 +0300

    Add 'cities' router with outline

--- simple-sales-backend/simple_sales/api/app.py
+++ simple-sales-backend/simple_sales/api/app.py
@@ -1,8 +1,9 @@
 import fastapi
 
-from simple_sales.api.routers import users, sessions
+from simple_sales.api.routers import cities, sessions, users
 
 app = fastapi.FastAPI()
 
+app.include_router(cities.router)
 app.include_router(users.router)
 app.include_router(sessions.router)
