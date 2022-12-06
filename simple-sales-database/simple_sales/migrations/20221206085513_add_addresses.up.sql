a77e13f7906cb27f121d665fd830d1d1075dda8b
Kirill Gashkov <k.e.gashkov@gmail.com>
2022-12-06 12:33:02 +0300

    Add 'addresses' table

---
+++ simple-sales-database/simple_sales/migrations/20221206085513_add_addresses.up.sql
@@ -0,0 +1,15 @@
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS addresses (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    postal_code text NOT NULL,
+    city_id uuid NOT NULL REFERENCES cities (id),
+    street text NOT NULL,
+    house text NOT NULL,
+    apartment text
+);
+
+ALTER TABLE clients ADD COLUMN IF NOT EXISTS organization_address_id uuid REFERENCES addresses (id);
+ALTER TABLE contacts ADD COLUMN IF NOT EXISTS address_id uuid REFERENCES addresses (id);
+
+COMMIT;
