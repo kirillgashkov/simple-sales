424340dc1adb43a45c1b6fbd518f8ec5282cee2f
Kirill Gashkov <kirill@gashkov.com>
2022-12-06 19:49:50 +0300

    Add 'reload' option to the 'api' command

--- simple-sales-backend/simple_sales/api/__main__.py
+++ simple-sales-backend/simple_sales/api/__main__.py
@@ -1,14 +1,15 @@
 import typer
 import uvicorn
 
-from simple_sales.api.app import app
-
 typer_app = typer.Typer()
 
 
 @typer_app.command()
-def api() -> None:
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+def api(
+    reload: bool = typer.Option(False, help="Reload the server when code changes.")
+) -> None:
+    # App is specified as a string to enable reloading.
+    uvicorn.run("simple_sales.api.app:app", host="0.0.0.0", port=8000, reload=reload)
 
 
 if __name__ == "__main__":
