c3823ac0512766261bc681464e739e072d439c85
Kirill Gashkov <kirill@gashkov.com>
2022-12-10 15:55:20 +0300

    Fix 'get_db' return type

--- simple-sales-backend/simple_sales/api/dependencies/db.py
+++ simple-sales-backend/simple_sales/api/dependencies/db.py
@@ -1,8 +1,10 @@
+from collections.abc import AsyncIterator
+
 import asyncpg
 
 from simple_sales.api.app import db
 
 
-async def get_db() -> asyncpg.Connection:
+async def get_db() -> AsyncIterator[asyncpg.Connection]:
     async with db.connection() as conn:
         yield conn
