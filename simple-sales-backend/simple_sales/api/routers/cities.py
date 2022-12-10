f2ab881f1d7d9585ad0338ff1854028fa981bae5
Kirill Gashkov <k.e.gashkov@gmail.com>
2022-12-10 14:38:16 +0300

    Implement 'GET /cities'

--- simple-sales-backend/simple_sales/api/routers/cities.py
+++ simple-sales-backend/simple_sales/api/routers/cities.py
@@ -1,8 +1,21 @@
-import fastapi
+from uuid import UUID
 
-router = fastapi.APIRouter()
+import asyncpg
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+
+from simple_sales.api.dependencies.db import get_db
+
+router = APIRouter()
 
 
-@router.get("/cities")
-def get_cities() -> None:
-    ...
+class CityOut(BaseModel):
+    id: UUID
+    city: str
+    region: str
+
+
+@router.get("/cities", response_model=list[CityOut])
+async def get_cities(db: asyncpg.Connection = Depends(get_db)) -> list[CityOut]:
+    records = await db.fetch("SELECT id, city, region FROM cities")
+    return [CityOut(**r) for r in records]
