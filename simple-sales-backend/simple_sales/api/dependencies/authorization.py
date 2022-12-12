713616bc723fdf81a269d2347e3c4a6bed78bcc0
Kirill Gashkov <k.e.gashkov@gmail.com>
2022-12-12 19:24:52 +0300

    Copy 'get_current_session()' to 'authorization'

--- simple-sales-backend/simple_sales/api/dependencies/authorization.py
+++ simple-sales-backend/simple_sales/api/dependencies/authorization.py
@@ -1,14 +1,42 @@
+from datetime import datetime
+from uuid import UUID
+
 import argon2
 from asyncpg import Connection
-from fastapi import Depends, HTTPException, status
+from fastapi import Cookie, Depends, HTTPException, status
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 from simple_sales.api.dependencies.db import get_db
-from simple_sales.api.dependencies.session import get_current_session
 from simple_sales.models.authorization import Authorization
 from simple_sales.models.session import Session
+from simple_sales.settings import API_SESSION_ID_COOKIE_NAME
 
 ph = argon2.PasswordHasher()
+
+
+async def get_current_session(
+    session_id: UUID | None = Cookie(None, alias=API_SESSION_ID_COOKIE_NAME),
+    db: Connection = Depends(get_db),
+) -> Session | None:
+    if session_id is None:
+        return None
+
+    record = await db.fetchrow(
+        """
+            SELECT
+                id, user_id, created_at, expires_at
+            FROM sessions
+            WHERE id = $1 AND expires_at > $2
+            LIMIT 1
+        """,
+        session_id,
+        datetime.utcnow(),
+    )
+
+    if record is None:
+        return None
+
+    return Session(*record)
 
 
 async def get_current_authorization(
