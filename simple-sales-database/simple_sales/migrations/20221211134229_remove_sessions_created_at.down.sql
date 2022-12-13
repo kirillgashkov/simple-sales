bd1148081bf02cea2bcbc63e9a6b05dd3a054c82
Kirill Gashkov <k.e.gashkov@gmail.com>
2022-12-13 13:51:08 +0300

    Make 'remove_sessions_created_at' more universal

--- simple-sales-database/simple_sales/migrations/20221211134229_remove_sessions_created_at.down.sql
+++ simple-sales-database/simple_sales/migrations/20221211134229_remove_sessions_created_at.down.sql
@@ -5,6 +5,7 @@ UPDATE sessions SET created_at = expires_at - interval '1 second';
 ALTER TABLE sessions ALTER COLUMN created_at SET NOT NULL;
 ALTER TABLE sessions ALTER COLUMN created_at SET DEFAULT now();
 
+ALTER TABLE sessions DROP CONSTRAINT IF EXISTS expires_at_after_created_at;
 ALTER TABLE sessions ADD CONSTRAINT expires_at_after_created_at CHECK (expires_at > created_at);
 
 COMMIT;
