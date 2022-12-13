46913784d81274cca1e6195b6877c1a27aef804a
Kirill Gashkov <kirill@gashkov.com>
2022-12-13 12:16:24 +0300

    Add 'EmployeeIn' model and update 'EmployeeOut'

--- simple-sales-backend/simple_sales/api/routers/employees.py
+++ simple-sales-backend/simple_sales/api/routers/employees.py
@@ -3,15 +3,24 @@ from uuid import UUID
 from fastapi import APIRouter
 from pydantic import BaseModel
 
-from simple_sales.api.routers.cities import CityOut
+from simple_sales.api.routers.cities import CityOut, CityInByID, CityInByData
+from simple_sales.api.routers.employee_types import EmployeeTypeOut, EmployeeTypeInByID
 
 router = APIRouter()
 
 
 class EmployeeOut(BaseModel):
     id: UUID
-    employee_type: str
+    employee_type: EmployeeTypeOut
     first_name: str
     middle_name: str | None
     last_name: str
     city: CityOut
+
+
+class EmployeeIn(BaseModel):
+    employee_type: EmployeeTypeInByID
+    first_name: str
+    middle_name: str | None
+    last_name: str
+    city: CityInByID | CityInByData
