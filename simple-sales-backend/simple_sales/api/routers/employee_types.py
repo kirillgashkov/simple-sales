c328a28c7d180889dc0ee7a316e668c26a962542
Kirill Gashkov <k.e.gashkov@gmail.com>
2022-12-13 20:13:30 +0300

    Rework 'employee_types'

--- simple-sales-backend/simple_sales/api/routers/employee_types.py
+++ simple-sales-backend/simple_sales/api/routers/employee_types.py
@@ -9,6 +9,21 @@ from simple_sales.api.dependencies.db import get_db
 router = APIRouter()
 
 
+class EmployeeType(BaseModel):
+    id: UUID
+    name: str
+
+
+async def select_employee_types(db: Connection) -> list[EmployeeType]:
+    records = await db.fetch(
+        """
+        SELECT id, name
+        FROM employee_types
+        """
+    )
+    return [EmployeeType(**r) for r in records]
+
+
 class EmployeeTypeOut(BaseModel):
     id: UUID
     name: str
@@ -20,5 +35,5 @@ class EmployeeTypeInReference(BaseModel):
 
 @router.get("/employee-types", response_model=list[EmployeeTypeOut])
 async def get_employee_types(db: Connection = Depends(get_db)) -> list[EmployeeTypeOut]:
-    records = await db.fetch("SELECT id, name FROM employee_types")
-    return [EmployeeTypeOut(**r) for r in records]
+    employee_types = await select_employee_types(db)
+    return [EmployeeTypeOut(**t.dict()) for t in employee_types]
