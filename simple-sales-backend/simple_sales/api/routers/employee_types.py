7491760af0fc81fc14141537c1ad714e5ed6a9fe
Kirill Gashkov <kirill@gashkov.com>
2022-12-13 12:16:02 +0300

    Add 'Employee*' models

---
+++ simple-sales-backend/simple_sales/api/routers/employee_types.py
@@ -0,0 +1,24 @@
+from uuid import UUID
+
+from asyncpg import Connection
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+
+from simple_sales.api.dependencies.db import get_db
+
+router = APIRouter()
+
+
+class EmployeeTypeOut(BaseModel):
+    id: UUID
+    name: str
+
+
+class EmployeeTypeInByID(BaseModel):
+    id: UUID
+
+
+@router.get("/employee-types", response_model=list[EmployeeTypeOut])
+async def get_employee_types(db: Connection = Depends(get_db)) -> list[EmployeeTypeOut]:
+    records = await db.fetch("SELECT id, name FROM employee_types")
+    return [EmployeeTypeOut(**r) for r in records]
