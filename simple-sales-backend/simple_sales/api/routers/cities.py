595cb09a6d9bd4cad6a4d29c6eb589e56a0169a4
Kirill Gashkov <k.e.gashkov@gmail.com>
2022-12-13 20:09:55 +0300

    Rework 'cities'

--- simple-sales-backend/simple_sales/api/routers/cities.py
+++ simple-sales-backend/simple_sales/api/routers/cities.py
@@ -9,14 +9,72 @@ from simple_sales.api.dependencies.db import get_db
 router = APIRouter()
 
 
+class City(BaseModel):
+    id: UUID
+    name: str
+    region: str | None
+
+
+async def insert_city(name: str, region: str, db: Connection) -> City | None:
+    record = await db.fetchrow(
+        """
+        INSERT INTO cities (name, region)
+        VALUES ($1, $2)
+        RETURNING id, name, region
+        """,
+        name,
+        region,
+    )
+    return City(**record) if record is not None else None
+
+
+async def select_cities(db: Connection) -> list[City]:
+    records = await db.fetch(
+        """
+        SELECT id, name, region
+        FROM cities
+        """
+    )
+    return [City(**r) for r in records]
+
+
+async def select_city_by_id(city_id: UUID, db: Connection) -> City | None:
+    record = await db.fetchrow(
+        """
+        SELECT id, name, region
+        FROM cities
+        WHERE id = $1
+        LIMIT 1
+        """,
+        city_id,
+    )
+    return City(**record) if record is not None else None
+
+
+async def select_city_by_name_and_region(
+    name: str, region: str | None, db: Connection
+) -> City | None:
+    record = await db.fetchrow(
+        """
+        SELECT id, name, region
+        FROM cities
+        WHERE name = $1 AND region = $2
+        LIMIT 1
+        """,
+        name,
+        region,
+    )
+    return City(**record) if record is not None else None
+
+
 class CityOut(BaseModel):
     id: UUID
-    city: str
+    name: str
     region: str | None
 
 
 class CityIn(BaseModel):
-    city: str
+    name: str
     region: str | None
 
 
@@ -26,5 +84,5 @@ class CityInReference(BaseModel):
 
 @router.get("/cities", response_model=list[CityOut])
 async def get_cities(db: Connection = Depends(get_db)) -> list[CityOut]:
-    records = await db.fetch("SELECT id, city, region FROM cities")
-    return [CityOut(**r) for r in records]
+    cities = await select_cities(db)
+    return [CityOut(**c.dict()) for c in cities]
