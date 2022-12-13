f499f7007d60d46c096d3b84b82456d64ff38860
Kirill Gashkov <kirill@gashkov.com>
2022-12-13 20:22:09 +0300

    Add 'Employee' model

--- simple-sales-backend/simple_sales/api/routers/employees.py
+++ simple-sales-backend/simple_sales/api/routers/employees.py
@@ -3,13 +3,23 @@ from uuid import UUID
 from fastapi import APIRouter
 from pydantic import BaseModel
 
-from simple_sales.api.routers.cities import CityIn, CityInReference, CityOut
+from simple_sales.api.routers.cities import City, CityIn, CityInReference, CityOut
 from simple_sales.api.routers.employee_types import (
+    EmployeeType,
     EmployeeTypeInReference,
     EmployeeTypeOut,
 )
 
 router = APIRouter()
+
+
+class Employee(BaseModel):
+    id: UUID
+    employee_type: EmployeeType
+    first_name: str
+    middle_name: str | None
+    last_name: str
+    city: City
 
 
 class EmployeeIn(BaseModel):
