37d8125b5a2b2cde55aade13ee8b6d21867de1b9
Kirill Gashkov <k.e.gashkov@gmail.com>
2022-12-13 03:30:41 +0300

    Implement 'get_current_user()'

--- simple-sales-backend/simple_sales/api/routers/employees.py
+++ simple-sales-backend/simple_sales/api/routers/employees.py
@@ -1,3 +1,17 @@
+from uuid import UUID
+
 from fastapi import APIRouter
+from pydantic import BaseModel
+
+from simple_sales.api.routers.cities import CityOut
 
 router = APIRouter()
+
+
+class EmployeeOut(BaseModel):
+    id: UUID
+    employee_type: str
+    first_name: str
+    middle_name: str | None
+    last_name: str
+    city: CityOut
