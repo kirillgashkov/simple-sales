8d353d582af7d603747a52497bb5fd36fc55ca17
Kirill Gashkov <kirill@gashkov.com>
2022-12-15 17:47:32 +0300

    Use 'select_user_by_id' in 'api.users'

--- simple-sales-backend/simple_sales/api/routers/users.py
+++ simple-sales-backend/simple_sales/api/routers/users.py
@@ -9,6 +9,7 @@ from simple_sales.api.dependencies.db import get_db
 from simple_sales.api.routers.cities import CityOut
 from simple_sales.api.routers.employee_types import EmployeeTypeOut
 from simple_sales.api.routers.employees import EmployeeOut
+from simple_sales.db.queries.users import select_user_by_id
 
 router = APIRouter()
 
@@ -23,50 +24,29 @@ async def get_current_user(
     user_id: UUID = Depends(get_current_user_id),
     db: Connection = Depends(get_db),
 ) -> UserOut:
-    user_out_record = await db.fetchrow(
-        """
-        SELECT
-            u.username,
-            e.id,
-            t.id as employee_type_id,
-            t.name as employee_type_name,
-            e.first_name,
-            e.middle_name,
-            e.last_name,
-            c.id as city_id,
-            c.city as city_name,
-            c.region as city_region
-        FROM users u
-        JOIN employees e ON u.employee_id = e.id
-        JOIN employee_types t ON e.employee_type_id = t.id
-        JOIN cities c ON e.city_id = c.id
-        WHERE u.id = $1
-        LIMIT 1
-        """,
-        user_id,
-    )
+    user = select_user_by_id(user_id, db)
 
-    if user_out_record is None:
+    if user is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="User not found",
         )
 
     return UserOut(
-        username=user_out_record["username"],
+        username=user.username,
         employee=EmployeeOut(
-            id=user_out_record["id"],
+            id=user.employee.id,
+            first_name=user.employee.first_name,
+            middle_name=user.employee.middle_name,
+            last_name=user.employee.last_name,
             employee_type=EmployeeTypeOut(
-                id=user_out_record["employee_type_id"],
-                name=user_out_record["employee_type_name"],
+                id=user.employee.employee_type.id,
+                name=user.employee.employee_type.name,
             ),
-            first_name=user_out_record["first_name"],
-            middle_name=user_out_record["middle_name"],
-            last_name=user_out_record["last_name"],
             city=CityOut(
-                id=user_out_record["city_id"],
-                city=user_out_record["city_name"],
-                region=user_out_record["city_region"],
+                id=user.employee.city.id,
+                name=user.employee.city.name,
+                region=user.employee.city.region,
             ),
         ),
     )
