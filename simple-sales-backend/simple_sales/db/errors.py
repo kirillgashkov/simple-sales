58010840a10adb24754521ddb1ffc8da3f91b3bb
Kirill Gashkov <kirill@gashkov.com>
2022-12-18 17:37:13 +0300

    Add 'InsertError's

--- simple-sales-backend/simple_sales/db/errors.py
+++ simple-sales-backend/simple_sales/db/errors.py
@@ -1,2 +1,14 @@
 class DatabaseError(Exception):
     pass
+
+
+class InsertError(DatabaseError):
+    pass
+
+
+class InsertDidNotReturnError(InsertError):
+    pass
+
+
+class SelectDidNotReturnAfterInsertError(InsertError):
+    pass
