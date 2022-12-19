a87087de701e58bbe28fab664ff4f2d5588c0dac
Kirill Gashkov <kirill@gashkov.com>
2022-12-19 04:29:04 +0300

    Add 'get_current_session()'

--- simple-sales-backend/simple_sales/api/dependencies/auth.py
+++ simple-sales-backend/simple_sales/api/dependencies/auth.py
@@ -12,3 +12,18 @@ from simple_sales.db.models import User, Session
 from simple_sales.db.queries.sessions import select_session
 from simple_sales.db.queries.users import select_user, update_user
 from simple_sales.settings import API_SESSION_ID_COOKIE_NAME
+
+
+async def get_current_session(
+    session_id: UUID | None = Cookie(None, alias=API_SESSION_ID_COOKIE_NAME),
+    db: Connection = Depends(get_db),
+) -> Session | None:
+    if not session_id:
+        return None
+
+    session = await select_session(db, id=session_id, expires_after=datetime.now())
+
+    if not session:
+        return None
+
+    return session
