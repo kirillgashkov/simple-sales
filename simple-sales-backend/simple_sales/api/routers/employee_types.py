27537f9e34d9fbcd6b0e11881488f574bc04af8f
Kirill Gashkov <k.e.gashkov@gmail.com>
2022-12-19 03:13:29 +0300

    Update 'cities', 'employee_types' and 'employees'

--- simple-sales-backend/simple_sales/api/routers/employee_types.py
+++ simple-sales-backend/simple_sales/api/routers/employee_types.py
@@ -1,25 +1,14 @@
-from uuid import UUID
-
 from asyncpg import Connection
 from fastapi import APIRouter, Depends
-from pydantic import BaseModel
 
 from simple_sales.api.dependencies.db import get_db
+from simple_sales.api.models import EmployeeTypeOut
+from simple_sales.db.queries.employee_types import select_employee_types
 
 router = APIRouter()
 
 
-class EmployeeTypeInReference(BaseModel):
-    id: UUID
-
-
-class EmployeeTypeOut(BaseModel):
-    id: UUID
-    name: str
-
-
 @router.get("/employee-types", response_model=list[EmployeeTypeOut])
 async def get_employee_types(db: Connection = Depends(get_db)) -> list[EmployeeTypeOut]:
-    # employee_types = await select_employee_types(db)
-    # return [EmployeeTypeOut(**t.dict()) for t in employee_types]
-    return []
+    employee_types = await select_employee_types(db)
+    return [EmployeeTypeOut(**t.dict()) for t in employee_types]
--- simple-sales-backend/simple_sales/api/routers/employees.py
+++ simple-sales-backend/simple_sales/api/routers/employees.py
@@ -1,29 +1,4 @@
-from uuid import UUID
-
 from fastapi import APIRouter
-from pydantic import BaseModel
 
-from simple_sales.api.routers.cities import CityIn, CityInReference, CityOut
-from simple_sales.api.routers.employee_types import (
-    EmployeeTypeInReference,
-    EmployeeTypeOut,
-)
 
 router = APIRouter()
-
-
-class EmployeeIn(BaseModel):
-    employee_type: EmployeeTypeInReference
-    first_name: str
-    middle_name: str | None
-    last_name: str
-    city: CityIn | CityInReference
-
-
-class EmployeeOut(BaseModel):
-    id: UUID
-    employee_type: EmployeeTypeOut
-    first_name: str
-    middle_name: str | None
-    last_name: str
-    city: CityOut
