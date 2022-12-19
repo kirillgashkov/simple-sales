3a1fcfb2d04154983e96c65eca0e89b5bf82b7b6
Kirill Gashkov <k.e.gashkov@gmail.com>
2022-12-19 19:00:23 +0300

    Add 'create_user()'

--- simple-sales-backend/simple_sales/api/routers/users.py
+++ simple-sales-backend/simple_sales/api/routers/users.py
@@ -1,10 +1,12 @@
-from asyncpg import Connection
-from fastapi import APIRouter, Depends
+import argon2
+from asyncpg import Connection, exceptions
+from fastapi import APIRouter, Depends, HTTPException, status
 
 from simple_sales.api.dependencies.db import get_db
 from simple_sales.api.dependencies.auth import (
     get_current_user as get_current_user_dependency,
 )
+from simple_sales.api.dependencies.argon2 import get_password_hasher
 from simple_sales.api.models import (
     CityOut,
     EmployeeOut,
@@ -19,8 +21,16 @@ from simple_sales.api.models import (
 from simple_sales.db.models import User, Employee
 from simple_sales.db.queries.employees import select_employee, insert_employee
 from simple_sales.db.queries.users import insert_user
+from simple_sales.db.queries.cities import select_or_insert_city
+from simple_sales.db.errors import UsernameAlreadyExistsError
 
 router = APIRouter()
+
+
+_HTTP_409_USERNAME_ALREADY_EXISTS = HTTPException(
+    status_code=status.HTTP_409_CONFLICT,
+    detail="Username already exists",
+)
 
 
 @router.get("/users/current", response_model=UserOut)
@@ -30,6 +40,47 @@ async def get_current_user(
 ) -> UserOut:
     employee = await select_employee(db, employee_id=current_user.employee_id)
     return _user_out_from_user_and_employee(current_user, employee)
+
+
+@router.post("/users", response_model=UserOut)
+async def create_user(
+    user_in: UserIn,
+    db: Connection = Depends(get_db),
+    ph: argon2.PasswordHasher = Depends(get_password_hasher),
+) -> UserOut:
+    async with db.transaction():
+        if isinstance(user_in.employee.city, CityIn):
+            city = await select_or_insert_city(
+                db,
+                name=user_in.employee.city.name,
+                region=user_in.employee.city.region,
+            )
+            city_id = city.id
+        elif isinstance(user_in.employee.city, CityInReference):
+            city_id = user_in.employee.city.id
+        else:
+            raise RuntimeError("Unexpected city type")
+
+        employee = await insert_employee(
+            db,
+            employee_type_id=user_in.employee.employee_type.id,
+            first_name=user_in.employee.first_name,
+            middle_name=user_in.employee.middle_name,
+            last_name=user_in.employee.last_name,
+            city_id=city_id,
+        )
+
+        try:
+            user = await insert_user(
+                db,
+                username=user_in.username,
+                password_hash=ph.hash(user_in.password),
+                employee_id=employee.id,
+            )
+        except UsernameAlreadyExistsError:
+            raise _HTTP_409_USERNAME_ALREADY_EXISTS
+
+    return _user_out_from_user_and_employee(user, employee)
 
 
 def _user_out_from_user_and_employee(user: User, employee: Employee) -> UserOut:
