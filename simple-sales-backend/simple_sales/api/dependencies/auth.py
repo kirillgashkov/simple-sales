a373e3c12d586641a56886d1bebc82a4f8f484b7
Kirill Gashkov <kirill@gashkov.com>
2022-12-19 04:29:40 +0300

    Add 'get_current_password_authorized_user()'

--- simple-sales-backend/simple_sales/api/dependencies/auth.py
+++ simple-sales-backend/simple_sales/api/dependencies/auth.py
@@ -27,3 +27,32 @@ async def get_current_session(
         return None
 
     return session
+
+
+async def get_current_password_authorized_user(
+    credentials: HTTPBasicCredentials | None = Depends(HTTPBasic()),
+    db: Connection = Depends(get_db),
+    ph: argon2.PasswordHasher = Depends(get_password_hasher),
+) -> User | None:
+    if not credentials:
+        return None
+
+    user = await select_user(db, username=credentials.username)
+
+    if not user:
+        return None
+
+    try:
+        ph.verify(user.password_hash, credentials.password)
+    except argon2.exceptions.VerifyMismatchError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+        )
+
+    if ph.check_needs_rehash(user.password_hash):
+        user = await update_user(
+            db, user_id=user.id, new_password_hash=ph.hash(credentials.password)
+        )
+
+    return user
