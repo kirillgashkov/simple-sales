826023b33098b1645ebade0c4755cc395f0c30ba
Kirill Gashkov <kirill@gashkov.com>
2022-12-19 03:13:29 +0300

    Update 'cities', 'employee_types' and 'employees'

--- simple-sales-backend/simple_sales/api/routers/cities.py
+++ simple-sales-backend/simple_sales/api/routers/cities.py
@@ -1,31 +1,14 @@
-from uuid import UUID
-
 from asyncpg import Connection
 from fastapi import APIRouter, Depends
-from pydantic import BaseModel
 
 from simple_sales.api.dependencies.db import get_db
+from simple_sales.api.models import CityOut
+from simple_sales.db.queries.cities import select_cities
 
 router = APIRouter()
 
 
-class CityIn(BaseModel):
-    name: str
-    region: str | None
-
-
-class CityInReference(BaseModel):
-    id: UUID
-
-
-class CityOut(BaseModel):
-    id: UUID
-    name: str
-    region: str | None
-
-
 @router.get("/cities", response_model=list[CityOut])
 async def get_cities(db: Connection = Depends(get_db)) -> list[CityOut]:
-    # cities = await select_cities(db)
-    # return [CityOut(**c.dict()) for c in cities]
-    return []
+    cities = await select_cities(db)
+    return [CityOut(**c.dict()) for c in cities]
