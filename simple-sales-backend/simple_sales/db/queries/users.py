a700ee31effac3dcf18be06b54ad6fc6b5aadabf
Kirill Gashkov <k.e.gashkov@gmail.com>
2022-12-21 00:42:24 +0300

    Fix 'insert_users()'

--- simple-sales-backend/simple_sales/db/queries/users.py
+++ simple-sales-backend/simple_sales/db/queries/users.py
@@ -94,14 +94,9 @@ async def insert_user(
     employee_last_name: str,
     employee_city_id: UUID,
 ) -> User:
-    query, *params = (
-        """
-        INSERT INTO users (
-            username,
-            password_hash,
-            employee_id
-        )
-        VALUES ($1, $2, (
+    async with db.transaction():
+        insert_employees_query, *insert_employees_params = (
+            """
             INSERT INTO employees (
                 employee_type_id,
                 first_name,
@@ -109,35 +104,54 @@ async def insert_user(
                 last_name,
                 city_id
             )
-            VALUES ($3, $4, $5, $6, $7)
+            VALUES ($1, $2, $3, $4, $5)
             RETURNING id
-        ))
-        RETURNING id
-        """,
-        username,
-        password_hash,
-        employee_type_id,
-        employee_first_name,
-        employee_middle_name,
-        employee_last_name,
-        employee_city_id,
-    )
+            """,
+            employee_type_id,
+            employee_first_name,
+            employee_middle_name,
+            employee_last_name,
+            employee_city_id,
+        )
 
-    try:
-        user_id = await db.fetchval(query, *params)
-    except exceptions.UniqueViolationError as e:
-        if e.constraint_name == "users_username_lower_idx":
-            raise UsernameAlreadyExistsError(username)
-        raise UniqueViolationError()
-    except exceptions.ForeignKeyViolationError as e:
-        if e.constraint_name == "employees_city_id_fkey":
-            raise ReferencedCityNotFoundError(employee_city_id)
-        elif e.constraint_name == "employees_employee_type_id_fkey":
-            raise ReferencedEmployeeTypeNotFoundError(employee_type_id)
-        raise ForeignKeyViolationError()
+        try:
+            employee_id = await db.fetchval(
+                insert_employees_query, *insert_employees_params
+            )
+        except exceptions.ForeignKeyViolationError as e:
+            if e.constraint_name == "employees_city_id_fkey":
+                raise ReferencedCityNotFoundError(employee_city_id)
+            elif e.constraint_name == "employees_employee_type_id_fkey":
+                raise ReferencedEmployeeTypeNotFoundError(employee_type_id)
+            raise ForeignKeyViolationError()
 
-    if not user_id:
-        raise InsertDidNotReturnError()
+        if not employee_id:
+            raise InsertDidNotReturnError()
+
+        insert_users_query, *insert_users_params = (
+            """
+            INSERT INTO users (
+                username,
+                password_hash,
+                employee_id
+            )
+            VALUES ($1, $2, $3)
+            RETURNING id
+            """,
+            username,
+            password_hash,
+            employee_id,
+        )
+
+        try:
+            user_id = await db.fetchval(insert_users_query, *insert_users_params)
+        except exceptions.UniqueViolationError as e:
+            if e.constraint_name == "users_username_lower_idx":
+                raise UsernameAlreadyExistsError(username)
+            raise UniqueViolationError()
+
+        if not user_id:
+            raise InsertDidNotReturnError()
 
     user = await select_user(db, user_id=user_id)
     if not user:
@@ -158,37 +172,11 @@ async def update_user(
     employee_city_id: UUID,
 ) -> User:
     async with db.transaction():
-        update_users_query, *update_users_params = (
-            """
-            UPDATE users
-            SET username = $1
-            WHERE id = $2
-            RETURNING id, employee_id
-            """,
-            username,
-            user_id,
-        )
-
-        try:
-            update_users_row = await db.fetchrow(
-                update_users_query, *update_users_params
-            )
-        except exceptions.UniqueViolationError as e:
-            if e.constraint_name == "users_username_lower_idx":
-                raise UsernameAlreadyExistsError(username)
-            raise UniqueViolationError()
-
-        if not update_users_row:
-            raise UpdateDidNotReturnError()
-
-        returned_user_id = update_users_row["id"]
-        returned_employee_id = update_users_row["employee_id"]
-
         update_employees_query, *update_employees_params = (
             """
             UPDATE employees
             SET employee_type_id = $1, first_name = $2, middle_name = $3, last_name = $4, city_id = $5
-            WHERE id = $6
+            WHERE id = (SELECT employee_id FROM users WHERE id = $6)
             RETURNING 1
             """,
             employee_type_id,
@@ -196,7 +184,7 @@ async def update_user(
             employee_middle_name,
             employee_last_name,
             employee_city_id,
-            returned_employee_id,
+            user_id,
         )
 
         try:
@@ -213,9 +201,32 @@ async def update_user(
         if not update_employees_row:
             raise UpdateDidNotReturnError()
 
-        user = await select_user(db, user_id=returned_user_id)
-        if not user:
-            raise SelectDidNotReturnAfterUpdateError()
+        update_users_query, *update_users_params = (
+            """
+            UPDATE users
+            SET username = $1
+            WHERE id = $2
+            RETURNING id
+            """,
+            username,
+            user_id,
+        )
+
+        try:
+            returned_user_id = await db.fetchval(
+                update_users_query, *update_users_params
+            )
+        except exceptions.UniqueViolationError as e:
+            if e.constraint_name == "users_username_lower_idx":
+                raise UsernameAlreadyExistsError(username)
+            raise UniqueViolationError()
+
+        if not returned_user_id:
+            raise UpdateDidNotReturnError()
+
+    user = await select_user(db, user_id=returned_user_id)
+    if not user:
+        raise SelectDidNotReturnAfterUpdateError()
 
     return user
 
