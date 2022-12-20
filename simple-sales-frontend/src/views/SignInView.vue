0a3c852b6145e59c5cf576e865376b38dc4abcff
Kirill Gashkov <kirill@gashkov.com>
2022-12-20 18:52:08 +0300

    Add 'SignInView'

--- simple-sales-frontend/src/views/SignInView.vue
+++ simple-sales-frontend/src/views/SignInView.vue
@@ -1,5 +1,48 @@
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+    }
+  },
+  methods: {
+    signIn() {
+    },
+  },
+}
+</script>
+
 <template>
-  <main>
-    Work in progress...
+  <main class="container">
+    <section class="section">
+      <div class="columns is-centered">
+        <div class="column is-half">
+          <h1 class="title has-text-centered">Вход</h1>
+
+          <form class="box" @submit.prevent="signIn">
+            <div class="field">
+              <label class="label">Имя пользователя</label>
+              <div class="control">
+                <input class="input" type="text" placeholder="ivanov" v-model="username">
+              </div>
+            </div>
+            <div class="field">
+              <label class="label">Пароль</label>
+              <div class="control">
+                <input class="input" type="password" placeholder="********" v-model="password">
+              </div>
+            </div>
+            <div class="field">
+              <div class="control">
+                <button class="button is-primary is-fullwidth">
+                  <strong>Войти</strong>
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
