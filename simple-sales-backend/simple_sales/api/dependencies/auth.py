833eaccb810334fee005a349a64ff2a4f83327d4
Kirill Gashkov <kirill@gashkov.com>
2022-12-20 20:26:16 +0300

    Renovate auth

--- simple-sales-backend/simple_sales/api/dependencies/auth.py
+++ simple-sales-backend/simple_sales/api/dependencies/auth.py
@@ -10,35 +10,39 @@ from simple_sales.api.dependencies.argon2 import get_password_hasher
 from simple_sales.api.dependencies.db import get_db
 from simple_sales.db.models import Session, User
 from simple_sales.db.queries.sessions import select_session
-from simple_sales.db.queries.users import select_user, update_user
+from simple_sales.db.queries.users import (
+    select_user,
+    select_user_password_hash_by_username,
+    update_user_password_hash,
+)
 from simple_sales.settings import API_SESSION_ID_COOKIE_NAME
 
 _AUTHENTICATE_HEADERS = {"WWW-Authenticate": "Basic"}
 
-_INVALID_USERNAME_OR_PASSWORD_EXCEPTION = HTTPException(
+_HTTP_401_INVALID_USERNAME_OR_PASSWORD_EXCEPTION = HTTPException(
     status_code=status.HTTP_401_UNAUTHORIZED,
     detail="Invalid username or password",
     headers=_AUTHENTICATE_HEADERS,
 )
 
-_INVALID_SESSION_ID_EXCEPTION = HTTPException(
+_HTTP_401_INVALID_SESSION_ID_EXCEPTION = HTTPException(
     status_code=status.HTTP_401_UNAUTHORIZED,
     detail="Invalid session ID",
 )
 
-_CREDENTIALS_AND_SESSION_ID_MATCH_DIFFERENT_USERS_EXCEPTION = HTTPException(
+_HTTP_401_CREDENTIALS_AND_SESSION_ID_MATCH_DIFFERENT_USERS_EXCEPTION = HTTPException(
     status_code=status.HTTP_401_UNAUTHORIZED,
     detail="Credentials and session ID match different users",
     headers=_AUTHENTICATE_HEADERS,
 )
 
-_CREDENTIALS_WERE_PROVIDED_FOR_ANOTHER_USER_EXCEPTION = HTTPException(
+_HTTP_401_CREDENTIALS_WERE_PROVIDED_FOR_ANOTHER_USER_EXCEPTION = HTTPException(
     status_code=status.HTTP_401_UNAUTHORIZED,
     detail="Credentials were provided for another user",
     headers=_AUTHENTICATE_HEADERS,
 )
 
-_NOT_AUTHENTICATED_EXCEPTION = HTTPException(
+_HTTP_401_NOT_AUTHENTICATED_EXCEPTION = HTTPException(
     status_code=status.HTTP_401_UNAUTHORIZED,
     detail="Not authenticated",
     headers=_AUTHENTICATE_HEADERS,
@@ -52,7 +56,7 @@ async def get_current_session(
     session = await _get_session_if_valid(session_id=session_id, db=db)
 
     if not session:
-        raise _INVALID_SESSION_ID_EXCEPTION
+        raise _HTTP_401_INVALID_SESSION_ID_EXCEPTION
 
     return session
 
@@ -64,7 +68,7 @@ async def get_current_user(
     ph: argon2.PasswordHasher = Depends(get_password_hasher),
 ) -> User:
     if credentials:
-        password_authorized_user = await _get_password_authorized_user(
+        password_authorized_user_id = await _get_password_authorized_user_id(
             credentials=credentials,
             db=db,
             ph=ph,
@@ -74,16 +78,20 @@ async def get_current_user(
         session = await _get_session_if_valid(session_id=session_id, db=db)
 
     if credentials and session:
-        if password_authorized_user.id != session.user.id:
-            raise _CREDENTIALS_AND_SESSION_ID_MATCH_DIFFERENT_USERS_EXCEPTION
+        if password_authorized_user_id != session.user_id:
+            raise _HTTP_401_CREDENTIALS_AND_SESSION_ID_MATCH_DIFFERENT_USERS_EXCEPTION
 
     if credentials:
-        return password_authorized_user
+        user_id = password_authorized_user_id
+    elif session:
+        user_id = session.user_id
+    else:
+        raise _HTTP_401_NOT_AUTHENTICATED_EXCEPTION
 
-    if session:
-        return session.user
+    user = await select_user(db, user_id=user_id)
+    assert user is not None
 
-    raise _NOT_AUTHENTICATED_EXCEPTION
+    return user
 
 
 async def verify_password_authorization_for_current_user(
@@ -92,36 +100,40 @@ async def verify_password_authorization_for_current_user(
     db: Connection = Depends(get_db),
     ph: argon2.PasswordHasher = Depends(get_password_hasher),
 ) -> None:
-    current_password_authorized_user = await _get_password_authorized_user(
+    current_password_authorized_user_id = await _get_password_authorized_user_id(
         credentials=credentials,
         db=db,
         ph=ph,
     )
 
-    if current_password_authorized_user.id != current_user.id:
-        raise _CREDENTIALS_WERE_PROVIDED_FOR_ANOTHER_USER_EXCEPTION
+    if current_password_authorized_user_id != current_user.id:
+        raise _HTTP_401_CREDENTIALS_WERE_PROVIDED_FOR_ANOTHER_USER_EXCEPTION
 
 
-async def _get_password_authorized_user(
+async def _get_password_authorized_user_id(
     *, credentials: HTTPBasicCredentials, db: Connection, ph: argon2.PasswordHasher
-) -> User:
-    user = await select_user(db, username=credentials.username)
+) -> UUID:
+    user_password_hash = await select_user_password_hash_by_username(
+        db, username=credentials.username
+    )
 
-    if not user:
-        raise _INVALID_USERNAME_OR_PASSWORD_EXCEPTION
+    if not user_password_hash:
+        raise _HTTP_401_INVALID_USERNAME_OR_PASSWORD_EXCEPTION
 
     try:
-        ph.verify(user.password_hash, credentials.password)
+        ph.verify(user_password_hash.password_hash, credentials.password)
     except argon2.exceptions.VerifyMismatchError:
-        raise _INVALID_USERNAME_OR_PASSWORD_EXCEPTION
+        raise _HTTP_401_INVALID_USERNAME_OR_PASSWORD_EXCEPTION
 
-    if ph.check_needs_rehash(user.password_hash):
-        user = await update_user(
-            db, user_id=user.id, new_password_hash=ph.hash(credentials.password)
+    if ph.check_needs_rehash(user_password_hash.password_hash):
+        user_password_hash = await update_user_password_hash(
+            db,
+            user_id=user_password_hash.user_id,
+            password_hash=ph.hash(credentials.password),
         )
 
-    return user
+    return user_password_hash.user_id
 
 
 async def _get_session_if_valid(*, session_id: UUID, db: Connection) -> Session | None:
-    return await select_session(db, id=session_id, expires_after=datetime.now())
+    return await select_session(db, session_id=session_id, expires_after=datetime.now())
