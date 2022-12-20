142b56adaf811b540cd60a70edd3fa03add2fb69
Kirill Gashkov <kirill@gashkov.com>
2022-12-21 01:29:31 +0300

    Add 'port' CLI option to API

--- simple-sales-backend/simple_sales/api/__main__.py
+++ simple-sales-backend/simple_sales/api/__main__.py
@@ -6,10 +6,11 @@ typer_app = Typer()
 
 @typer_app.command()
 def api(
-    reload: bool = Option(False, help="Reload the server when code changes.")
+    port: int = Option(8000, help="Port to run the server on."),
+    reload: bool = Option(False, help="Reload the server when code changes."),
 ) -> None:
     # App is specified as a string to enable reloading.
-    uvicorn.run("simple_sales.api.app:app", host="0.0.0.0", port=8000, reload=reload)
+    uvicorn.run("simple_sales.api.app:app", host="0.0.0.0", port=port, reload=reload)
 
 
 if __name__ == "__main__":
