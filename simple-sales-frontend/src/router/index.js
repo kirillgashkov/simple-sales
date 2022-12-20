ba75e3ce44aec2506b1b897ce9bbb66eb012d19b
Kirill Gashkov <kirill@gashkov.com>
2022-12-20 18:07:34 +0300

    Add routes and views

--- simple-sales-frontend/src/router/index.js
+++ simple-sales-frontend/src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,21 +6,73 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: HomeView,
+      component: () => import("../views/HomeView.vue"),
+    },
+    {
+      path: "/",
+      name: "sign-in",
+      component: () => import("../views/SignInView.vue"),
+    },
+    {
+      path: "/",
+      name: "sign-up",
+      component: () => import("../views/SignUpView.vue"),
+    },
+    {
+      path: "/",
+      name: "account",
+      component: () => import("../views/AccountView.vue"),
     },
     {
       path: "/tasks",
       name: "tasks",
-      redirect: "/tasks/assigned",
+      component: () => import("../views/TasksView.vue"),
     },
-    // {
-    //   path: "/about",
-    //   name: "about",
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import("../views/AboutView.vue"),
-    // },
+    {
+      path: "/tasks/:id",
+      name: "task",
+      component: () => import("../views/TaskView.vue"),
+      props: true,
+    },
+    {
+      path: "/clients",
+      name: "clients",
+      component: () => import("../views/ClientsView.vue"),
+    },
+    {
+      path: "/clients/:id",
+      name: "client",
+      component: () => import("../views/ClientView.vue"),
+      props: true,
+    },
+    {
+      path: "/clients/:clientID/contacts/:contactID",
+      name: "contact",
+      component: () => import("../views/ContactView.vue"),
+      props: true,
+    },
+    {
+      path: "/clients/:clientID/contracts/:contractID",
+      name: "contract",
+      component: () => import("../views/ContractView.vue"),
+      props: true,
+    },
+    {
+      path: "/employees",
+      name: "employees",
+      component: () => import("../views/EmployeesView.vue"),
+    },
+    {
+      path: "/employees/:id",
+      name: "employee",
+      component: () => import("../views/EmployeeView.vue"),
+      props: true,
+    },
+    {
+      path: "/reports",
+      name: "reports",
+      component: () => import("../views/ReportsView.vue"),
+    },
   ],
 });
 
