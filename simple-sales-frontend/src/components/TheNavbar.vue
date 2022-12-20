d3e8904952eb93e63d9ac551d8286e921a2a132d
Kirill Gashkov <k.e.gashkov@gmail.com>
2022-12-20 17:08:51 +0300

    Replace the default app with my; add 'TheNavbar'

---
+++ simple-sales-frontend/src/components/TheNavbar.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <nav class="navbar is-light" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <RouterLink class="navbar-item" to="/">
+        <img src="@/assets/logo.svg" width="84" height="28">
+      </RouterLink>
+
+      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+        <div class="navbar-item has-dropdown is-hoverable">
+          <RouterLink class="navbar-link is-arrowless" to="/tasks">
+            Задания
+          </RouterLink>
+
+          <div class="navbar-dropdown">
+            <RouterLink class="navbar-item" to="/tasks/assigned">
+              Назначенные
+            </RouterLink>
+            <RouterLink class="navbar-item" to="/tasks/created">
+              Созданные
+            </RouterLink>
+            <RouterLink class="navbar-item" to="/tasks/all">
+              Все
+            </RouterLink>
+          </div>
+        </div>
+
+        <RouterLink class="navbar-item" to="/clients">
+          Клиенты
+        </RouterLink>
+
+        <RouterLink class="navbar-item" to="/employees">
+          Сотрудники
+        </RouterLink>
+
+        <RouterLink class="navbar-item" to="/reports">
+          Отчеты
+        </RouterLink>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>Зарегистрироваться</strong>
+            </a>
+            <a class="button">
+              Войти
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
