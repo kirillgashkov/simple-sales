da7e12c4e319785a33ab65a3759b6694f11e6be6
Kirill Gashkov <k.e.gashkov@gmail.com>
2022-12-22 01:14:21 +0300

    Add group roles and some user roles

---
+++ simple-sales-database/simple_sales/create_roles.sql
@@ -0,0 +1,57 @@
+CREATE ROLE simple_sales_admin
+WITH
+    NOINHERIT
+    CREATEDB
+    CREATEROLE
+    BYPASSRLS;
+
+CREATE ROLE simple_sales_manager
+WITH
+    NOINHERIT;
+
+CREATE ROLE simple_sales_salesperson
+WITH
+    NOINHERIT;
+
+
+CREATE ROLE it_guy WITH
+    INHERIT
+    LOGIN
+    PASSWORD '123';
+
+GRANT it_guy TO simple_sales_admin;
+
+CREATE ROLE michael WITH
+    INHERIT
+    LOGIN
+    PASSWORD '123';
+
+GRANT michael TO simple_sales_manager;
+
+CREATE ROLE jan WITH
+    INHERIT
+    LOGIN
+    PASSWORD '123';
+
+GRANT jan TO simple_sales_manager;
+
+CREATE ROLE dwight WITH
+    INHERIT
+    LOGIN
+    PASSWORD '123';
+
+GRANT dwight TO simple_sales_salesperson;
+
+CREATE ROLE jim WITH
+    INHERIT
+    LOGIN
+    PASSWORD '123';
+
+GRANT jim TO simple_sales_salesperson;
+
+CREATE ROLE pam WITH
+    INHERIT
+    LOGIN
+    PASSWORD '123';
+
+GRANT pam TO simple_sales_salesperson;
