9cc995120ff1a6e1e1a979914710bf709b7969df
Kirill Gashkov <kirill@gashkov.com>
2022-12-22 02:17:37 +0300

    Grant 'all' database privileges to admin

--- simple-sales-database/simple_sales/_revoke_privileges.sql
+++ simple-sales-database/simple_sales/_revoke_privileges.sql
@@ -1,5 +1,28 @@
--- simple_sales_admin
+BEGIN;
 
--- simple_sales_manager
+--
+-- Role: "simple_sales_salesperson"
+--
 
--- simple_sales_salesperson
+
+--
+-- Role: "simple_sales_manager"
+--
+
+
+--
+-- Role: "simple_sales_admin"
+--
+
+
+-- WARNING: The following commands must be executed from the "simple_sales"
+-- database.
+
+REVOKE ALL PRIVILEGES ON ALL ROUTINES IN SCHEMA public FROM simple_sales_admin;
+REVOKE ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public FROM simple_sales_admin;
+REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA public FROM simple_sales_admin;
+REVOKE ALL PRIVILEGES ON SCHEMA public FROM simple_sales_admin;
+
+REVOKE ALL PRIVILEGES ON DATABASE simple_sales FROM simple_sales_admin;
+
+COMMIT;
