ceb601c3be81a98f439a0d920c1bb4171e2afe02
Kirill Gashkov <k.e.gashkov@gmail.com>
2022-12-22 03:07:11 +0300

    Add another group role 'employee'

--- simple-sales-database/simple_sales/create_roles.sql
+++ simple-sales-database/simple_sales/create_roles.sql
@@ -4,24 +4,39 @@ WITH
     CREATEDB
     CREATEROLE;
 
-CREATE ROLE simple_sales_manager
+
+CREATE ROLE simple_sales_employee
 WITH
     NOINHERIT;
 
+
+CREATE ROLE simple_sales_manager
+WITH
+    INHERIT;
+
+GRANT simple_sales_employee TO simple_sales_manager;
+
+
 CREATE ROLE simple_sales_salesperson
 WITH
-    NOINHERIT;
+    INHERIT;
+
+GRANT simple_sales_employee TO simple_sales_salesperson;
+
+
+-- Temporary roles for development.
 
 
 CREATE ROLE it_guy WITH
     INHERIT
     LOGIN
     PASSWORD '123'
-    -- CREATE DB and CREATE ROLE are not inherited by default
+    -- CREATE DB and CREATE ROLE are not inherited by default.
     CREATEDB
     CREATEROLE;
 
 GRANT simple_sales_admin TO it_guy;
+
 
 CREATE ROLE michael WITH
     INHERIT
@@ -30,12 +45,14 @@ CREATE ROLE michael WITH
 
 GRANT simple_sales_manager TO michael;
 
+
 CREATE ROLE jan WITH
     INHERIT
     LOGIN
     PASSWORD '123';
 
 GRANT simple_sales_manager TO jan;
+
 
 CREATE ROLE dwight WITH
     INHERIT
@@ -44,12 +61,14 @@ CREATE ROLE dwight WITH
 
 GRANT simple_sales_salesperson TO dwight;
 
+
 CREATE ROLE jim WITH
     INHERIT
     LOGIN
     PASSWORD '123';
 
 GRANT simple_sales_salesperson TO jim;
+
 
 CREATE ROLE pam WITH
     INHERIT
