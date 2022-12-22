129fb0ce45b5f634cc2c463b53c20d9402e2107a
Kirill Gashkov <kirill@gashkov.com>
2022-12-22 19:34:19 +0300

    Implement policies

---
+++ simple-sales-database/simple_sales/_drop_policies.sql
@@ -0,0 +1,13 @@
+BEGIN;
+
+DROP POLICY IF EXISTS admin_tasks_policy ON tasks;
+
+DROP POLICY IF EXISTS manager_delete_tasks_policy ON tasks;
+DROP POLICY IF EXISTS manager_insert_tasks_policy ON tasks;
+DROP POLICY IF EXISTS manager_update_tasks_policy ON tasks;
+DROP POLICY IF EXISTS manager_select_tasks_policy ON tasks;
+
+DROP POLICY IF EXISTS employee_update_tasks_policy ON tasks;
+DROP POLICY IF EXISTS employee_select_tasks_policy ON tasks;
+
+COMMIT;
