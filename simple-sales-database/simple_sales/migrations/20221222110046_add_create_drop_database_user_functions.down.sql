85f6ac0cbcd2c3b408ecf5bf2619f472487512fb
Kirill Gashkov <k.e.gashkov@gmail.com>
2022-12-22 15:34:01 +0300

    Add create/drop database user functions

---
+++ simple-sales-database/simple_sales/migrations/20221222110046_add_create_drop_database_user_functions.down.sql
@@ -0,0 +1,7 @@
+BEGIN;
+
+DROP FUNCTION IF EXISTS drop_database_user;
+
+DROP FUNCTION IF EXISTS create_database_user;
+
+COMMIT;
--- simple-sales-database/simple_sales/schema.sql
+++ simple-sales-database/simple_sales/schema.sql
@@ -31,17 +31,6 @@ COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UU
 
 
 --
--- Name: get_current_employee_id(); Type: FUNCTION; Schema: public; Owner: -
---
-
-CREATE FUNCTION public.get_current_employee_id() RETURNS uuid
-    LANGUAGE sql
-    AS $$
-    SELECT employee_id FROM database_users WHERE role_name = current_user;
-$$;
-
-
---
 -- Name: is_valid_role_name(name); Type: FUNCTION; Schema: public; Owner: -
 --
 
@@ -57,6 +46,105 @@ $$;
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
+
+--
+-- Name: database_users; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.database_users (
+    role_name name NOT NULL,
+    employee_id uuid NOT NULL,
+    CONSTRAINT valid_role_name CHECK (public.is_valid_role_name(role_name))
+);
+
+
+--
+-- Name: create_database_user(name, text, text, text, text, text, text, text); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION public.create_database_user(database_username name, database_password text, employee_type_name text, first_name text, middle_name text, last_name text, city_name text, city_region text DEFAULT NULL::text) RETURNS SETOF public.database_users
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+    employee_type_id uuid;
+    city_id uuid;
+    employee_id uuid;
+BEGIN
+    SELECT id INTO STRICT employee_type_id FROM employee_types WHERE name = employee_type_name;
+
+    IF city_region IS NULL THEN
+        SELECT id INTO STRICT city_id FROM cities WHERE name = city_name;
+    ELSE
+        SELECT id INTO STRICT city_id FROM cities WHERE name = city_name AND region = city_region;
+    END IF;
+
+    INSERT INTO employees (employee_type_id, first_name, middle_name, last_name, city_id)
+    VALUES (employee_type_id, first_name, middle_name, last_name, city_id)
+    RETURNING id INTO STRICT employee_id;
+
+    EXECUTE format('CREATE ROLE %I WITH INHERIT LOGIN PASSWORD %L', database_username, database_password);
+
+    IF employee_type_name = 'manager' THEN
+        EXECUTE format('GRANT simple_sales_manager TO %I', database_username);
+    ELSIF employee_type_name = 'salesperson' THEN
+        EXECUTE format('GRANT simple_sales_salesperson TO %I', database_username);
+    ELSE
+        RAISE EXCEPTION 'Unknown employee type: %', employee_type_name;
+    END IF;
+
+    INSERT INTO database_users (role_name, employee_id)
+    VALUES (database_username, employee_id);
+
+    RETURN QUERY SELECT * FROM database_users WHERE role_name = database_username;
+END;
+$$;
+
+
+--
+-- Name: drop_database_user(name); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION public.drop_database_user(database_username name) RETURNS SETOF public.database_users
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+    database_user database_users;
+    employee_type_name text;
+BEGIN
+    DELETE FROM database_users WHERE role_name = database_username RETURNING * INTO STRICT database_user;
+
+    SELECT employee_types.name INTO STRICT employee_type_name
+    FROM employees
+    JOIN employee_types ON employee_types.id = employees.employee_type_id
+    WHERE employees.id = database_user.employee_id;
+
+    IF employee_type_name = 'manager' THEN
+        EXECUTE format('REVOKE simple_sales_manager FROM %I', database_username);
+    ELSIF employee_type_name = 'salesperson' THEN
+        EXECUTE format('REVOKE simple_sales_salesperson FROM %I', database_username);
+    ELSE
+        RAISE EXCEPTION 'Unknown employee type: %', employee_type_name;
+    END IF;
+
+    EXECUTE format('DROP ROLE %I', database_username);
+
+    DELETE FROM employees WHERE id = database_user.employee_id;
+
+    RETURN NEXT database_user;
+END;
+$$;
+
+
+--
+-- Name: get_current_employee_id(); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION public.get_current_employee_id() RETURNS uuid
+    LANGUAGE sql
+    AS $$
+    SELECT employee_id FROM database_users WHERE role_name = current_user;
+$$;
+
 
 --
 -- Name: addresses; Type: TABLE; Schema: public; Owner: -
@@ -138,17 +226,6 @@ CREATE TABLE public.contracts (
 CREATE TABLE public.contracts_products (
     contract_number text NOT NULL,
     product_serial_number text NOT NULL
-);
-
-
---
--- Name: database_users; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.database_users (
-    role_name name NOT NULL,
-    employee_id uuid NOT NULL,
-    CONSTRAINT valid_role_name CHECK (public.is_valid_role_name(role_name))
 );
 
 
