6c21684b30bfb62e971c4e00365f93dc28daf21f
Kirill Gashkov <k.e.gashkov@gmail.com>
2024-04-04 03:47:44 +0300

    Make 'cities' always have some rows

--- database/simple_sales/migrations/20221205142931_add_cities_addresses.up.sql
+++ database/simple_sales/migrations/20221205142931_add_cities_addresses.up.sql
@@ -7,6 +7,12 @@ CREATE TABLE IF NOT EXISTS cities (
     city_name text NOT NULL,
     region_name text
 );
+INSERT INTO cities (id, city_name, region_name)
+VALUES
+    ('16a23ce3-a0f1-49aa-8aa3-34189692aa75', 'Москва', 'Россия'),
+    ('6ccf2941-382b-4272-a89a-fe28bf768e49', 'Санкт-Петербург', 'Россия')
+ON CONFLICT DO NOTHING;
+
 
 CREATE TABLE IF NOT EXISTS addresses (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
